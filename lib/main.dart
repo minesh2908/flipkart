4a2859e3069d5cb16733c5922947cc29230301bd
minesh2908 <54062823+minesh2908@users.noreply.github.com>
2023-06-24 20:03:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      //debugShowCheckedModeBanner: false,
       routes: {
         '/':(context) => HomeScreen(),
         'productDetailPage':(context)=> ProductDetailPage()
